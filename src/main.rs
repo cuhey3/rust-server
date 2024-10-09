fb6370646dec4c65ea680cf557dc639b653ad322
cuhey3 <uotaneet@gmail.com>
2024-10-09 13:23:17 +0900

    update server for multi channel

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,3 @@
-//! Example chat application.
-//!
-//! Run with
-//!
-//! ```not_rust
-//! cargo run -p example-chat
-//! ```
-
 use axum::{
     extract::{
         ws::{Message, WebSocket, WebSocketUpgrade},
@@ -16,22 +8,43 @@ use axum::{
     Router,
 };
 use futures::{sink::SinkExt, stream::StreamExt};
+use serde::{Deserialize, Serialize};
 use std::{
     collections::HashSet,
     env,
     sync::{Arc, Mutex},
 };
 use tokio::sync::broadcast;
+use tokio::sync::broadcast::Sender;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 // Our shared state
 struct AppState {
-    // We require unique usernames. This tracks which usernames have been taken.
-    user_set: Mutex<HashSet<String>>,
     // Channel used to send messages to all connected clients.
-    tx: broadcast::Sender<String>,
+    tx: Sender<String>,
+    channel_list: Mutex<Vec<String>>,
+    channel_tx_list: Mutex<Vec<Sender<String>>>,
 }
 
+#[derive(Deserialize, Debug)]
+struct ChannelUser {
+    user_name: String,
+    channel_name: String,
+}
+
+#[derive(Serialize)]
+struct ChannelMessage {
+    user_name: String,
+    message_type: MessageType,
+    message: String,
+}
+
+#[derive(Serialize)]
+enum MessageType {
+    Message,
+    Join,
+    Left,
+}
 #[tokio::main]
 async fn main() {
     tracing_subscriber::registry()
@@ -43,10 +56,15 @@ async fn main() {
         .init();
 
     // Set up application state for use with with_state().
-    let user_set = Mutex::new(HashSet::new());
+    let channel_list: Mutex<Vec<String>> = Mutex::new(vec![]);
     let (tx, _rx) = broadcast::channel(100);
+    let channel_tx_list = Mutex::new(vec![tx.clone()]);
 
-    let app_state = Arc::new(AppState { user_set, tx });
+    let app_state = Arc::new(AppState {
+        tx,
+        channel_list,
+        channel_tx_list,
+    });
 
     let app = Router::new()
         .route("/", get(index))
@@ -82,26 +100,46 @@ async fn websocket_handler(
 // connected client / user, for which we will spawn two independent tasks (for
 // receiving / sending chat messages).
 async fn websocket(stream: WebSocket, state: Arc<AppState>) {
-    // By splitting, we can send and receive at the same time.
     let (mut sender, mut receiver) = stream.split();
 
     // Username gets set in the receive loop, if it's valid.
-    let mut username = String::new();
-    // Loop until a text message is found.
+    let mut channel_user = ChannelUser {
+        user_name: "".to_string(),
+        channel_name: "".to_string(),
+    };
+    let mut channel_index = 0_usize;
+    let (_tx, _rx) = broadcast::channel(100);
+    let mut state_tx: Sender<String> = _tx;
     while let Some(Ok(message)) = receiver.next().await {
         if let Message::Text(name) = message {
-            break;
+            let requested: Result<ChannelUser, _> = serde_json::from_str(&*name);
+            if requested.is_ok() {
+                channel_user = requested.unwrap();
+                state_tx = check_channel(&state, &channel_user.channel_name);
+                break;
+            } else {
+                // Only send our client that request is invalid.
+                let _ = sender
+                    .send(Message::Text(String::from("Invalid request. e.g. {\"user_name\": \"name\", \"channel_name\": \"name\"}")))
+                    .await;
+
+                return;
+            }
         }
     }
-
     // We subscribe *before* sending the "joined" message, so that we will also
     // display it to our client.
-    let mut rx = state.tx.subscribe();
+    let mut rx = state_tx.subscribe();
 
     // Now send the "joined" message to all subscribers.
-    let msg = format!("{username} joined.");
+    let channel_message = ChannelMessage {
+        user_name: channel_user.user_name.to_owned(),
+        message_type: MessageType::Join,
+        message: format!("{} joined.", channel_user.user_name.to_owned()),
+    };
+    let msg = serde_json::to_string(&channel_message).unwrap();
     tracing::debug!("{msg}");
-    let _ = state.tx.send(msg);
+    let _ = state_tx.send(msg);
 
     // Spawn the first task that will receive broadcast messages and send text
     // messages over the websocket to our client.
@@ -113,18 +151,22 @@ async fn websocket(stream: WebSocket, state: Arc<AppState>) {
             }
         }
     });
-
     // Clone things we want to pass (move) to the receiving task.
-    let tx = state.tx.clone();
-    let name = username.clone();
+    let tx = state_tx.clone();
+    let name = channel_user.user_name.clone();
 
     // Spawn a task that takes messages from the websocket, prepends the user
     // name, and sends them to all broadcast subscribers.
     let mut recv_task = tokio::spawn(async move {
         while let Some(Ok(Message::Text(text))) = receiver.next().await {
             // Add username before message.
-            // let _ = tx.send(format!("{name}: {text}"));
-            let _ = tx.send(text);
+            let channel_message = ChannelMessage {
+                user_name: name.to_owned(),
+                message_type: MessageType::Message,
+                message: text,
+            };
+            let message = serde_json::to_string(&channel_message).unwrap();
+            let _ = tx.send(message);
         }
     });
 
@@ -135,22 +177,34 @@ async fn websocket(stream: WebSocket, state: Arc<AppState>) {
     };
 
     // Send "user left" message (similar to "joined" above).
-    let msg = format!("{username} left.");
+    let channel_message = ChannelMessage {
+        user_name: channel_user.user_name.to_owned(),
+        message_type: MessageType::Left,
+        message: format!("{} left.", channel_user.user_name),
+    };
+    let msg = serde_json::to_string(&channel_message).unwrap();
     tracing::debug!("{msg}");
-    let _ = state.tx.send(msg);
-
-    // Remove username from map so new clients can take it again.
-    state.user_set.lock().unwrap().remove(&username);
+    let _ = state_tx.send(msg);
 }
 
-fn check_username(state: &AppState, string: &mut String, name: &str) {
-    let mut user_set = state.user_set.lock().unwrap();
+fn check_channel(state: &AppState, channel_name: &String) -> Sender<String> {
+    let mut channel_list = state.channel_list.lock().unwrap();
 
-    if !user_set.contains(name) {
-        user_set.insert(name.to_owned());
-
-        string.push_str(name);
+    if !channel_list.contains(&channel_name) {
+        channel_list.push(channel_name.to_owned());
+        let mut channel_tx_list = state.channel_tx_list.lock().unwrap();
+        let (tx, _rx) = broadcast::channel(100);
+        channel_tx_list.push(tx);
     }
+
+    let channel_index = channel_list
+        .iter()
+        .enumerate()
+        .find(|(_, name)| *name == channel_name)
+        .unwrap()
+        .0;
+    let mut channel_tx_list = state.channel_tx_list.lock().unwrap();
+    channel_tx_list[channel_index].clone()
 }
 
 // Include utf-8 file at **compile** time.
